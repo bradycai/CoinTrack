d0730f339f3804222fa06ff0f8a48bad73b90808
Brady <bradycai26@gmail.com>
2025-09-15 20:04:44 -0400

    Initial Commit

---
+++ CoinTrack/CoinTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinTrackApp.swift
+//  CoinTrack
+//
+//  Created by Brady Cai on 9/15/25.
+//
+
+import SwiftUI
+
+@main
+struct CoinTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoinTrackTests/CoinTrackTests.swift
@@ -0,0 +1,17 @@
+//
+//  CoinTrackTests.swift
+//  CoinTrackTests
+//
+//  Created by Brady Cai on 9/15/25.
+//
+
+import Testing
+@testable import CoinTrack
+
+struct CoinTrackTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
